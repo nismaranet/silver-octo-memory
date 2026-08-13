c16baa9b10b588a84b87246862d8ff6746f8808a
Muhammad Hazmi <mhazmiee@gmail.com>
2026-08-13 21:41:43 +0700

    Sanity Deploy

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'kla6oibcylcunw66mxnaad7q',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
